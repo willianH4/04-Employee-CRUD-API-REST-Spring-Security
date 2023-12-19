553812e3d3aed4909dbf9ebc6952e88a591bc34e
Willian <williamhernandez450@gmail.com>
2023-12-19 09:33:32 -0600

    Implement JDBC Authentication and Spring security

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,39 +1,25 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 	
+	// add support for JDBC ... no more hardcoded users :-)
+	
 	@Bean
-	public InMemoryUserDetailsManager userDetailManager() {
-		UserDetails john = User.builder()
-				.username("john")
-				.password("{noop}test123")
-				.roles("EMPLOYEE")
-				.build();
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
 		
-		UserDetails mary = User.builder()
-				.username("mary")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER")
-				.build();
-		
-		UserDetails susan = User.builder()
-				.username("susan")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "ADMIN", "MANAGER")
-				.build();
-		
-		return new InMemoryUserDetailsManager(john, mary, susan);
+		return new JdbcUserDetailsManager(dataSource);
 	}
 	
 	@Bean
@@ -59,5 +45,31 @@ public class DemoSecurityConfig {
 		return http.build();
 		
 	}
+	
+	// manage user in memory local
+	/**
+	@Bean
+	public InMemoryUserDetailsManager userDetailManager() {
+		UserDetails john = User.builder()
+				.username("john")
+				.password("{noop}test123")
+				.roles("EMPLOYEE")
+				.build();
+		
+		UserDetails mary = User.builder()
+				.username("mary")
+				.password("{noop}test123")
+				.roles("EMPLOYEE", "MANAGER")
+				.build();
+		
+		UserDetails susan = User.builder()
+				.username("susan")
+				.password("{noop}test123")
+				.roles("EMPLOYEE", "ADMIN", "MANAGER")
+				.build();
+		
+		return new InMemoryUserDetailsManager(john, mary, susan);
+	}
+	**/
 
 }
